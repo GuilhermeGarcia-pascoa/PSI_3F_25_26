9ea9d2bf81a631759ee99425de060621322840c6
GuilhermeGarcia-pascoa <paschoal2310@gmail.com>
2025-10-01 18:06:20 +0100

    Ola

---
+++ Aula01/Projeto01/X1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Projeto01
+{
+    internal class X1
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Olaa..... ");
+        }
+    }
+}
